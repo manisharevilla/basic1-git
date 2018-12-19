5772b75d417e76a916312a69bed93ab1892ff371
manisharevilla <manu4sekhar@gmail.com>
2018-12-19 12:20:15 +0530

    todays program

---
+++ js/all.js
@@ -0,0 +1,37 @@
+// var a = 6;
+// var b ="6";
+// console.log(a==b);
+
+// var a = 6;
+// var b ="6";
+// console.log(a===b);
+
+// var c=8;
+// var d=9;
+// var e=d;
+// console.log(e);
+// var e=14;
+// console.log(e);
+// console.log(d);
+
+// var first=[1,2];
+// var second=[1,2]
+// console.log(first==second);
+// console.log(first===second);
+
+// And condition
+var x=6;
+var y=4;
+console.log(x<10 && y>2);
+console.log(x<10 && y<2);
+
+// or condition
+var x=6;
+var y=4;
+console.log(x<10 ||y>2);
+console.log(x<10 || y<2);
+
+// not condition
+var x=6;
+var y=4;
+console.log(!(x==y));
